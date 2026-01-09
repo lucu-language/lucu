c35533670c41ffccbd5d34d50cddfb30faaea3dd
Astavie <astavie@pm.me>
2026-01-09 14:03:13 +0100

    also color unknown keyword red

--- lucu/src/stage/token/ansi.rs
+++ lucu/src/stage/token/ansi.rs
@@ -1,6 +1,6 @@
 use anstyle::{AnsiColor, Color};
 
-use crate::stage::token::TokenEnum;
+use crate::stage::token::{Keyword, TokenEnum};
 
 pub const KEYWORD: Color = Color::Ansi(AnsiColor::Cyan);
 pub const LITERAL: Color = Color::Ansi(AnsiColor::BrightGreen);
@@ -10,10 +10,10 @@ pub const UNKNOWN: Color = Color::Ansi(AnsiColor::Red);
 impl TokenEnum {
     pub fn color(self) -> Option<Color> {
         match self {
+            TokenEnum::Keyword(Keyword::Unknown) | TokenEnum::Unknown => Some(UNKNOWN),
             TokenEnum::Keyword(_) => Some(KEYWORD),
             TokenEnum::Literal(_) => Some(LITERAL),
             TokenEnum::Open(_) | TokenEnum::Close(_) | TokenEnum::Symbol(_) => Some(SYMBOL),
-            TokenEnum::Unknown => Some(UNKNOWN),
             TokenEnum::Identifier => None,
             TokenEnum::Eof => None,
         }
